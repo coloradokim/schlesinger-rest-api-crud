07b4c56e38f8a75df7f9a52a0ec06bae87073363
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-08-03 11:57:07 -0600

    post route working

--- routes/swords.js
+++ routes/swords.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var db = require('monk')('localhost/swords');
+var Swords = db.get('swords');
 
 router.get('/', function (req, res) {
   res.status(200).json({message: 'rawr! it is working!'});
+});
+
+/*create a new sword*/
+router.post('/', function(req, res) {
+  Swords.insert(req.body, function(err, sword) {
+    if (err) {
+      res.send(err);
+    }
+    res.status(201).json(sword)
+  });
 });
 
 module.exports = router
