80e53af8b98a6e2e633079b5eaf61c37a4db4083
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-08-03 14:12:21 -0600

    working GET and PUT routes

--- routes/swords.js
+++ routes/swords.js
@@ -3,11 +3,12 @@ var router = express.Router();
 var db = require('monk')('localhost/swords');
 var Swords = db.get('swords');
 
+
 router.get('/', function (req, res) {
   res.status(200).json({message: 'rawr! it is working!'});
 });
 
-/*create a new sword*/
+/* create a new sword */
 router.post('/', function(req, res) {
   Swords.insert(req.body, function(err, sword) {
     if (err) {
@@ -16,5 +17,36 @@ router.post('/', function(req, res) {
     res.status(201).json(sword)
   });
 });
+
+
+// router.get('/', function (req, res) {
+//   Swords.find({}, function(err, swords) {
+//     if (err) {
+//       res.send(err);
+//     }
+//     res.status(200).json(swords);
+//   })
+// });
+
+/* Get One Sword */
+router.get('/:id', function (req, res) {
+  Swords.findOne({_id: req.params.id}, function (err, sword) {
+    if (err) {
+      res.send(err)
+    }
+    res.status(200).json(sword)
+  })
+});
+
+/* Update a sword with new information */
+router.put('/:id', function (req, res) {
+  Swords.findAndModify({_id: req.params.id}, req.body, function (err, sword) {
+    if (err) {
+      throw err
+    }
+    res.json(req.body)
+  })
+});
+
 
 module.exports = router
